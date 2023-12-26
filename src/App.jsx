ca10b73ac55f525351d69cfc1b6f52a4bcdb8c4a
gosndule <80468848+gosndule@users.noreply.github.com>
2023-12-26 23:32:48 +0100

    main layout implemented

--- src/App.jsx
+++ src/App.jsx
@@ -10,38 +10,71 @@ const images = [1, 2, 3]
 const realImgs = [img1, img1, img1, img1, img1, img1, img1, img1, img1]
 
 function App() {
-  const carouselRef = useRef()
-  const imgRef = useRef()
+  const [isScrollingRight, setIsScrollingRight] = useState(false)
+  const [isScrollingLeft, setIsScrollingLeft] = useState(false)
 
-  useEffect(() => {
-    const beginningOfCenterImg = imgRef.current.offsetLeft
-    const centerImgWidth = imgRef.current.getBoundingClientRect().width
-    const containerWidth = carouselRef.current.getBoundingClientRect().width
-    const amountNeededToScroll =
-      beginningOfCenterImg - containerWidth / 2 + centerImgWidth / 2
+  const handleScrollRight = () => {
+    setIsScrollingRight(true)
+    setTimeout(() => {
+      setIsScrollingRight(false)
+    }, 3000)
+  }
 
-    carouselRef.current.scrollTo(amountNeededToScroll, 0)
-  }, [carouselRef, imgRef])
+  const handleScrollLeft = () => {
+    setIsScrollingLeft(true)
+    setTimeout(() => {
+      setIsScrollingLeft(false)
+    }, 3000)
+  }
+
+  // useEffect(() => {}, [isScrollingRight, isScrollingLeft])
+
+  const img = img1
+
+  const scrollingClasses = isScrollingRight
+    ? 'scrollRight '
+    : isScrollingLeft
+    ? 'scrollLeft'
+    : undefined
 
   return (
     <>
       <div id="container">
-        <div id="carousel" ref={carouselRef}>
-          {realImgs.map((img, idx) => {
-            return (
-              <img
-                key={Math.random() + 1}
-                src={img}
-                alt=""
-                className={`img ${idx === 4 ? 'blur' : undefined}`}
-                ref={idx === 4 ? imgRef : null}
-              />
-            )
-          })}
+        <div id="carousel">
+          <img src={img} className="base" />
+          <img src={img} className={`left3 sepia ${scrollingClasses}`} />
+          <img src={img} className={`left2 sepia ${scrollingClasses}`} />
+          <img src={img} className={`left1 blur ${scrollingClasses}`} />
+          <img src={img} className={`middle grayscale ${scrollingClasses}`} />
+          <img src={img} className={`right1 blur ${scrollingClasses}`} />
+          <img src={img} className={`right2 sepia ${scrollingClasses}`} />
+          <img src={img} className={`right3 sepia ${scrollingClasses}`} />
+          {/* <div className="ghost-carousel items-center flex">
+            <img src={img} className="base" />
+            <img
+              src={img}
+              id="ghostLeft"
+              className={`left1 blur ${scrollingClasses}`}
+            />
+            <img
+              src={img}
+              id="ghostRight"
+              className={`right1 blur ${scrollingClasses}`}
+            />
+          </div> */}
         </div>
       </div>
-      <button onClick={() => carouselRef.current.scrollTo(200, 0)}>
-        click me
+      <button
+        className="bg-white text-black px-10 py-3"
+        onClick={handleScrollLeft}
+      >
+        left
+      </button>
+      <button
+        className="bg-white text-black px-10 py-3"
+        onClick={handleScrollRight}
+      >
+        right
       </button>
     </>
   )
