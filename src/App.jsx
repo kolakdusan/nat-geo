4318afbb64d30a2fa2b8c544fa372d159f4fd17d
gosndule <80468848+gosndule@users.noreply.github.com>
2023-12-27 11:53:12 +0100

    WORKS!

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,55 @@ import img1 from './assets/lifeBelowZero.jpg'
 import img2 from './assets/lostCities.jpg'
 import img3 from './assets/incredibleDrPol.jpg'
 
-const repetitions = [1, 2, 3]
-const images = [1, 2, 3]
-// const realImgs = [img1, img1, img1, img2, img3, img3, img3]
-const realImgs = [img1, img1, img1, img1, img1, img1, img1, img1, img1]
+import imgData from './data'
+const TRANSITION_TIME = 600
+const VISIBLE_IMG_COUNT = 7
+const IMG_COUNT = imgData.length
 
 function App() {
   const [isScrollingRight, setIsScrollingRight] = useState(false)
   const [isScrollingLeft, setIsScrollingLeft] = useState(false)
+  const [activeImgIdx, setActiveImgIdx] = useState(Math.floor(IMG_COUNT / 2))
+
+  const [activeImgsIdxs, setActiveImgsIdxs] = useState(() => {
+    const initialActiveImgsIdxs = Array.from(
+      { length: VISIBLE_IMG_COUNT },
+      (_, idx) => activeImgIdx + idx - Math.floor(VISIBLE_IMG_COUNT / 2)
+    )
+    return initialActiveImgsIdxs
+  })
+
+  // const [imgWindow, setImgWindow] = useState(imgData.reduce((_, idx) => {
+  //   idx
+  // }))
 
   const handleScrollRight = () => {
+    setIsScrollingLeft(false)
     setIsScrollingRight(true)
     setTimeout(() => {
       setIsScrollingRight(false)
-    }, 3000)
+      setActiveImgIdx((prevState) => prevState + 1)
+    }, TRANSITION_TIME)
   }
 
   const handleScrollLeft = () => {
+    setIsScrollingRight(false)
     setIsScrollingLeft(true)
     setTimeout(() => {
       setIsScrollingLeft(false)
-    }, 3000)
+      setActiveImgIdx((prevState) => prevState - 1)
+    }, TRANSITION_TIME)
   }
 
-  // useEffect(() => {}, [isScrollingRight, isScrollingLeft])
+  useEffect(() => {
+    // This will be executed after activeImgIdx is updated
+    // Update activeImgsIdxs based on the new value of activeImgIdx
+    setActiveImgsIdxs((prevActiveImgsIdxs) =>
+      prevActiveImgsIdxs.map(
+        (_, idx) => activeImgIdx + idx - Math.floor(VISIBLE_IMG_COUNT / 2)
+      )
+    )
+  }, [activeImgIdx]) // Run this effect whenever activeImgIdx changes
 
   const img = img1
 
@@ -42,37 +67,83 @@ function App() {
       <div id="container">
         <div id="carousel">
           <img src={img} className="base" />
-          <img src={img} className={`left3 sepia ${scrollingClasses}`} />
+          {activeImgsIdxs.map((imgIdx, idx) => {
+            let classes = ''
+            switch (idx) {
+              case 0: {
+                classes = 'left3 '
+                break
+              }
+              case 1: {
+                classes = 'left2 '
+                break
+              }
+              case 2: {
+                classes = 'left1 '
+                break
+              }
+              case 3: {
+                classes = 'middle '
+                break
+              }
+              case 4: {
+                classes = 'right1 '
+                break
+              }
+              case 5: {
+                classes = 'right2 '
+                break
+              }
+              case 6: {
+                classes = 'right3 '
+                break
+              }
+              default: {
+                classes = 'middle '
+                break
+              }
+            }
+            classes += `${scrollingClasses}`
+            return (
+              <img
+                key={imgData[imgIdx]}
+                src={imgData[imgIdx]}
+                className={classes}
+              />
+            )
+          })}
+          {/* <img src={img} className={`left3 sepia ${scrollingClasses}`} />
           <img src={img} className={`left2 sepia ${scrollingClasses}`} />
           <img src={img} className={`left1 blur ${scrollingClasses}`} />
           <img src={img} className={`middle grayscale ${scrollingClasses}`} />
           <img src={img} className={`right1 blur ${scrollingClasses}`} />
           <img src={img} className={`right2 sepia ${scrollingClasses}`} />
-          <img src={img} className={`right3 sepia ${scrollingClasses}`} />
-          {/* <div className="ghost-carousel items-center flex">
-            <img src={img} className="base" />
+          <img src={img} className={`right3 sepia ${scrollingClasses}`} /> */}
+        </div>
+        <div className="absolute h-full border-2 border-green-500 top-0 left-0 w-full">
+          <div id="ghost-carousel">
             <img
-              src={img}
-              id="ghostLeft"
-              className={`left1 blur ${scrollingClasses}`}
+              src={imgData[activeImgIdx - 1]}
+              className={`left1 ${scrollingClasses}`}
             />
             <img
-              src={img}
-              id="ghostRight"
-              className={`right1 blur ${scrollingClasses}`}
+              src={imgData[activeImgIdx + 1]}
+              className={`right1 ${scrollingClasses}`}
             />
-          </div> */}
+          </div>
         </div>
       </div>
       <button
         className="bg-white text-black px-10 py-3"
         onClick={handleScrollLeft}
+        disabled={isScrollingLeft || isScrollingRight}
       >
         left
       </button>
       <button
         className="bg-white text-black px-10 py-3"
         onClick={handleScrollRight}
+        disabled={isScrollingLeft || isScrollingRight}
       >
         right
       </button>
---
+++ src/data.js
@@ -0,0 +1,33 @@
+import img0 from './assets/cesar911.jpg'
+import img1 from './assets/combatRescue.jpg'
+import img2 from './assets/generationX.jpg'
+import img3 from './assets/heartlandDocs.jpg'
+import img4 from './assets/incredibleDrPol.jpg'
+import img5 from './assets/lawlessIsland.jpg'
+import img6 from './assets/lionKingdom.jpg'
+import img7 from './assets/lifeBelowZero.jpg'
+import img8 from './assets/mars.jpg'
+import img9 from './assets/portProtection.jpg'
+import img10 from './assets/savageKingdom.jpg'
+import img11 from './assets/wild24.jpg'
+import img12 from './assets/wildRussia.jpg'
+import img13 from './assets/amazingPlanet.jpg'
+import img14 from './assets/lostCities.jpg'
+
+export default [
+  img0,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+]
