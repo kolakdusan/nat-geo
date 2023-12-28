f928677b4b0207323df957f6019b82a5d0eea705
gosndule <80468848+gosndule@users.noreply.github.com>
2023-12-28 09:26:39 +0100

    Works 28dec

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 
-import img1 from './assets/lifeBelowZero.jpg'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 import imgData from './data'
+import Image from './components/Image'
 // const TRANSITION_TIME = 600
 const TRANSITION_TIME = 600
 const VISIBLE_IMG_COUNT = 7
@@ -30,7 +31,7 @@ function App() {
     setIsScrollingRight(true)
     setTimeout(() => {
       setIsScrollingRight(false)
-      setActiveImgIdx((prevState) => prevState + 1)
+      setActiveImgIdx((prevState) => (prevState + 1) % IMG_COUNT)
     }, TRANSITION_TIME)
   }
 
@@ -39,14 +40,16 @@ function App() {
     setIsScrollingLeft(true)
     setTimeout(() => {
       setIsScrollingLeft(false)
-      setActiveImgIdx((prevState) => prevState - 1)
+      setActiveImgIdx((prevState) => (prevState - 1 + IMG_COUNT) % IMG_COUNT)
     }, TRANSITION_TIME)
   }
 
   useEffect(() => {
     setActiveImgsIdxs((prevActiveImgsIdxs) =>
       prevActiveImgsIdxs.map(
-        (_, idx) => activeImgIdx + idx - Math.floor(VISIBLE_IMG_COUNT / 2)
+        (_, idx) =>
+          (activeImgIdx + idx - Math.floor(VISIBLE_IMG_COUNT / 2) + IMG_COUNT) %
+          IMG_COUNT
       )
     )
   }, [activeImgIdx])
@@ -70,34 +73,29 @@ function App() {
               />
             )
           })}
-          <img src={imgData[activeImgIdx - 1]} className={`ghost-cover left`} />
           <img
-            src={imgData[activeImgIdx + 1]}
+            src={imgData[(activeImgIdx - 1 + IMG_COUNT) % IMG_COUNT]}
+            className={`ghost-cover left`}
+          />
+          <img
+            src={imgData[(activeImgIdx + 1) % IMG_COUNT]}
             className={`ghost-cover right`}
           />
+          <button
+            className="left-52 chevron"
+            onClick={handleScrollLeft}
+            disabled={isScrollingLeft || isScrollingRight}
+          >
+            <FaChevronLeft />
+          </button>
+          <button
+            className="right-52 chevron"
+            onClick={handleScrollRight}
+            disabled={isScrollingLeft || isScrollingRight}
+          >
+            <FaChevronRight />
+          </button>
         </div>
-        <div className="absolute h-full border-2 border-green-500 top-0 left-0 w-full">
-          <div className={`ghost-container ${scrollingClasses}`}>
-            <img src={imgData[activeImgIdx - 1]} className={`ghost-cover`} />
-            <img src={imgData[activeImgIdx + 1]} className={`ghost-cover`} />
-          </div>
-        </div>
-      </div>
-      <div className="w-full flex justify-end -mt-20">
-        <button
-          className="bg-white text-black px-10 py-3 z-[1000] "
-          onClick={handleScrollLeft}
-          disabled={isScrollingLeft || isScrollingRight}
-        >
-          left
-        </button>
-        <button
-          className="bg-white text-black px-10 py-3 z-[1000] "
-          onClick={handleScrollRight}
-          disabled={isScrollingLeft || isScrollingRight}
-        >
-          right
-        </button>
       </div>
     </>
   )
