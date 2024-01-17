dfdb393d34d76ae29852ce33c1470b78a9dba529
gosndule <80468848+gosndule@users.noreply.github.com>
2024-01-17 15:39:27 +0100

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/Header/Header'
 import Home from './pages/Home/Home.jsx'
 
 function App() {
-  const { showModal, setShowModal } = useNatGeo()
+  const { showModal } = useNatGeo()
 
   return (
     <AnimatePresence>
