5878ca82a8e3927f68dc520a99f2100086e04969
gosndule <80468848+gosndule@users.noreply.github.com>
2024-01-18 08:47:59 +0100

    gonna try fixing scrollbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,18 +6,11 @@ import NatGeoTv from './components/NatGeoTv'
 import HomeContainer from './components/HomeContainer'
 import DisneyPlusSlider from './components/DisneyPlusSlider'
 
-import styled from 'styled-components'
-
-const WhiteStuffDelete = styled.div.attrs({
-  className: 'w-[120%] h-[100px] ml-5 bg-white mt-[120px] mb-[-130px]',
-})``
-
 const Home = () => {
   return (
     <BasicMotionDivWrapper>
       <HomeContainer>
         <LatestStories />
-        {/* <WhiteStuffDelete /> */}
         <BgVideo>
           <BgVideoContent />
         </BgVideo>
